31c38b621a0a64759564a684f73e767253368d9b
ashwani33 <yadav.ashwani33@gmail.com>
2020-08-11 17:08:19 +0530

    Day 3 Assignments

---
+++ src/com/company/Day3_Integer_till20.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Day3_Integer_till20 {
+    public static void main(String[] args) {
+
+        int i=0;
+        while ( i<=20 )
+        {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
---
+++ src/com/company/Day3_Table_6.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Day3_Table_6 {
+
+    public static void main(String[] args) {
+
+        int num=6;
+        for(int i=1 ;i<=10 ;i++)
+            System.out.println(num+ "X" + i +" = "+ num*i);
+    }
+}
