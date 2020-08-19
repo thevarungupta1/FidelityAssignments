2e3d1ac8fc77a793bee96bb1a7cc122ba4310538
Himanshu Arora <Himanshu.arora590@gmail.com>
2020-08-19 14:31:37 +0530

    New Scripts

---
+++ src/com/company/Tableof6.java
@@ -0,0 +1,15 @@
+
+public class Tableof6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+		int k = 6;
+		for (int i =1; i <= 10; i++)
+		{
+			System.out.println(k + "*" + i + "=" + k*i );
+		}
+
+	}
+
+}
---
+++ src/com/company/cube.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class cube {
+
+	public static void main(String[] args)
+	{
+		System.out.println("Enter a number");
+		
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		System.out.println("Cube of number is : " + a*a*a );
+		
+	}	
+	
+	
+}
