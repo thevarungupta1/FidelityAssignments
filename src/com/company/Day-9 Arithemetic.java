f46a25d1f2d512cf1faa9b635f7e02585f64cf8d
Nehasahu14 <69468902+Nehasahu14@users.noreply.github.com>
2020-08-25 23:45:16 +0530

    Adding Day-9 code

---
+++ src/com/company/Day-9 Arithemetic.java
@@ -0,0 +1,18 @@
+
+public class Arithemetic {
+
+
+
+	public int add(int a, int b) {
+		int sum=a+b;
+		System.out.println(sum);
+		return sum;
+	}
+}
+
+class Adder extends Arithemetic{
+	public static void main(String[] args) {
+		Adder a =new Adder();
+		a.add(10, 20);
+ }
+}
